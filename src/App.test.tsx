7fb09ec8597b35e6cbe233d8926e0e36090ff273
Orajiaku Chukwudalu <chukwudalurjk@gmail.com>
2021-06-10 23:41:08 -0400

    patch: enabled build and test for all branches

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Group Home Admin', () => {
+test('renders Authenticating', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Group Home Admin/i);
+  const linkElement = screen.getByText(/Authenticating/i);
   expect(linkElement).toBeInTheDocument();
 });
