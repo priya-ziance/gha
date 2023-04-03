35db6dc7ca63f3f6bd522700cd5600fc59c0044b
howard britton <developerera77@gmail.com>
2023-04-03 12:03:04 +0530

    client id make dynamic

--- src/pages/dashboard/add-client/staffWitnessForm.tsx
+++ src/pages/dashboard/add-client/staffWitnessForm.tsx
@@ -1,178 +1,119 @@
-import { Classes, Intent } from "@blueprintjs/core";
-import { Tooltip2 } from "@blueprintjs/popover2";
-import { Button, Col, Dialog, Row } from "../../../components";
-import { FIELDS } from "./constants";
+import { Classes } from "@blueprintjs/core";
+import { Dialog, Table } from "../../../components";
 import { IDialog } from "./types";
+import { useContext, useEffect, useState } from "react";
 import { IStaffWithnessModel } from "../../../types";
-import { useContext, useState } from "react";
-import ToastsContext from "../../../contexts/toasts";
-import * as helpers from "../add-staff-witness/helpers";
-import api from "../../../api";
-import { Formik, FormikHelpers } from "formik";
-import formikWrapper from "../../../wrappers/formik";
-import OmniContactsInput from "../../../controlled-components/OmniContactInput";
 import ClientContext from "../../../contexts/client";
+import api from "../../../api";
+import * as helpers from "../../../utils/helpers";
+import { actionColumn, addressColumn, emailColumn, mobileColumn, nameColumn } from "../staff-witness/helpers";
+import URLS from "../../../utils/urls";
+
+const PAGE_SIZE = 10;
 
 const StaffWitnessForm = (props: IDialog) => {
   const { isOpen, handleClose } = props;
-  const [isOmniOpen, setIsOmniOpen] = useState(false);
-  const [selectedMedical, setSelectedMedical] = useState<
-    IStaffWithnessModel | undefined
-  >(undefined);
+  const [staffWitness, setStaffWitness] = useState<IStaffWithnessModel[] | []>(
+    []
+  );
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(false);
   const { id: clientId } = useContext(ClientContext);
-  const { addToast } = useContext(ToastsContext);
-  let initialValues = helpers.initialValues;
 
-  const handleSelectStaffWitnessClose = () => {
-    setIsOmniOpen(false);
+  const hasNextPage = staffWitness.length === PAGE_SIZE;
+  const hasPrevPage = page > 0;
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+
+      try {
+        setStaffWitness(
+          await api.staffWitness.getStaffWitness(clientId, {
+            page,
+            pageSize: PAGE_SIZE,
+          })
+        );
+      } catch (e: any) {}
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 200);
+    })();
+  }, [clientId, page]);
+
+  const onNextPage = () => {
+    if (hasNextPage) {
+      setPage((page) => page + 1);
+    }
   };
 
-  const onSubmit = async (values: any, options: FormikHelpers<any>) => {
-    const { resetForm, setSubmitting } = options;
-    setSubmitting(true);
-    try {
-      values.emp_id = clientId;
-      values.image = "dasd";
-
-      await api.staffWitness.createStaffWitness(values);
-      addToast({
-        message: "Staff Witness Created",
-        intent: Intent.SUCCESS,
-      });
-      handleClose();
-      // Reset the form
-      resetForm();
-    } catch (e: any) {
-      addToast({
-        message: "Something went wrong",
-        intent: Intent.DANGER,
-      });
+  const onPrevPage = () => {
+    if (hasPrevPage) {
+      setPage((page) => page - 1);
     }
-
-    setSubmitting(false);
   };
 
   return (
     <Dialog
       icon="info-sign"
       onClose={handleClose}
-      title="Add Staff Witness Form"
+      title="Staff Witness"
       isOpen={isOpen}
     >
       <>
-        <div className={`${Classes.DIALOG_BODY} add-client__levelsOfService`}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={helpers.validationSchema}
-            onSubmit={onSubmit}
-          >
-            {formikWrapper(
-              ({
-                wrapperProps: {
-                  getDateInputFormGroup,
-                  getSelectFieldInputFormGroup,
-                  getInputFormGroup,
-                  getAutocompleteInputFormGroup,
-                  getPhoneInputFormGroup,
-                },
-                formikProps: {
-                  handleSubmit,
-                  isSubmitting,
-                  setFieldValue,
-                  validateForm,
-                },
-              }) => {
-                return (
-                  <form onSubmit={handleSubmit}>
-                    <OmniContactsInput
-                      isOpen={isOmniOpen}
-                      onClose={handleSelectStaffWitnessClose}
-                      onSelect={(staffWitness: IStaffWithnessModel) => {
-                        setFieldValue("first_name", staffWitness.firstName);
-                        setFieldValue("last_name", staffWitness.lastName);
-                        setFieldValue("address", staffWitness.address);
-                        setFieldValue("mobile", staffWitness.mobile);
-                        setFieldValue("email", staffWitness.email);
-                        setFieldValue("hired_date", staffWitness.hiredDate);
-                        setFieldValue("location", staffWitness.location);
-                        validateForm();
-                        setIsOmniOpen(false);
-                        setSelectedMedical(staffWitness);
-                      }}
-                    />
-
-                    <Row>
-                      <Col xs={12} md={6}>
-                        {getInputFormGroup("first_name", {
-                          childProps: { disabled: !!selectedMedical },
-                        })}
-                      </Col>
-                      <Col xs={12} md={6}>
-                        {getInputFormGroup("last_name", {
-                          childProps: { disabled: !!selectedMedical },
-                        })}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col xs={12} md={6}>
-                        {getInputFormGroup("email", {
-                          childProps: { disabled: !!selectedMedical },
-                        })}
-                      </Col>
-
-                      <Col xs={12} md={6}>
-                        {getPhoneInputFormGroup("mobile", {
-                          childProps: {
-                            type: "tel",
-                            disabled: !!selectedMedical,
-                          },
-                        })}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col xs={6}>
-                        {getInputFormGroup("location", {
-                          childProps: { disabled: !!selectedMedical },
-                        })}
-                      </Col>
-                      <Col xs={6}>
-                        {getDateInputFormGroup("hired_date", {
-                          childProps: {
-                            type: "date",
-                            disabled: !!selectedMedical,
-                          },
-                        })}
-                      </Col>
-                    </Row>
-
-                    <Row>
-                      <Col xs={12} md={12}>
-                        {getAutocompleteInputFormGroup("address", {
-                          childProps: { disabled: !!selectedMedical },
-                        })}
-                      </Col>
-                    </Row>
-
-                    <div className="add-staff-witness__submit-container">
-                      <Tooltip2 content="This button is hooked up to close the dialog.">
-                        <Button onClick={handleClose}>Close</Button>
-                      </Tooltip2>
-                      &nbsp;
-                      <Button
-                        type="submit"
-                        disabled={isSubmitting}
-                        loading={isSubmitting}
-                        intent={Intent.PRIMARY}
-                      >
-                        Submit
-                      </Button>
-                    </div>
-                  </form>
-                );
-              },
-              FIELDS
-            )}
-          </Formik>
+        <div
+          className={`${Classes.DIALOG_BODY} add-client__levelsOfService`}
+        >
+          <Table
+                  loading={loading}
+                  numRows={staffWitness.length}
+                  getCellClipboardData={(row: any, col: any) => {
+                    return staffWitness[row];
+                  }}
+                  columns={[
+                    {
+                      title: "Name",
+                      cellRenderer: nameColumn,
+                      width: helpers.getTableWith(0.25),
+                    },
+                    {
+                      title: "Email",
+                      cellRenderer: emailColumn,
+                      width: helpers.getTableWith(0.25),
+                    },
+                    {
+                      title: "Mobile",
+                      cellRenderer: mobileColumn,
+                      width: helpers.getTableWith(0.2),
+                    },
+                    {
+                      title: "Address",
+                      cellRenderer: addressColumn,
+                      width: helpers.getTableWith(0.2),
+                    },
+                    {
+                      title: "Actions",
+                      cellRenderer: (data: any) => {
+                        return actionColumn(data, {
+                          viewLink: URLS.getPagePath("edit-staff-witness", {
+                            clientId,
+                            clientContactId: data.id,
+                          }),
+                        });
+                      },
+                      width: helpers.getTableWith(0.1),
+                    },
+                  ]}
+                  data={staffWitness}
+                  enableRowHeader={false}
+                  hasNextPage={hasNextPage}
+                  hasPrevPage={hasPrevPage}
+                  onNextPage={onNextPage}
+                  onPrevPage={onPrevPage}
+                  page={page}
+                  emptyTableMessage="No Staff Witness Found"
+                />
         </div>
       </>
     </Dialog>
